7f265218b4490cce2a2e0a942d51be50bf682276
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-05-04 23:41:59 +0100

    Add files via upload

--- dataStructure/src/graph/IGraph.java
+++ dataStructure/src/graph/IGraph.java
@@ -5,11 +5,10 @@ import java.util.Map;
 import java.util.Set;
 
 public interface IGraph {
+    void add(int from, int to, int distance);
     void add(int from, int to);
-    void add(int from, int to, Integer distance);
-    Integer getDistance(int from, int to);
-    // <노드, 차수의 수>
-    Map<Integer, Integer> getIndegree();
+    int getDistance(int from, int to);
+    Map<Integer, Integer> getIndegrees();
     Set<Integer> getVertexes();
     List<Integer> getNodes(int vertex);
 }
